ece91bd48cf733da89feff525a29df89c1ddb93c
Spidi Ritsu <spidiritsu@gmail.com>
2016-11-28 21:34:30 +0100

    poprawa tajtla

--- GermanExam/jsOld.js
+++
@@ -1,9 +0,0 @@
-function startup() {
-  $("#mainContainer").removeClass("hide").fadeOut(1);
-  setTimeout(function(){
-    $("#startup").fadeOut(500);
-    setTimeout(function(){
-      $("#mainContainer").fadeIn(500);
-    }, 500);
-  }, 500);
-}
