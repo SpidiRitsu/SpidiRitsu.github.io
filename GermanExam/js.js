99b4fc93787c65b7657c31fc6a416179f5cf599d
Spidi Ritsu <spidiritsu@gmail.com>
2016-11-28 21:42:07 +0100

    again.... and again....

--- GermanExam/js.js
+++ GermanExam/js.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
   });
   $(".btn").click(function() {
     $(this).addClass("hide");
-    $("#mainContainer").append("<div style='margin: 20px 10px; border: 2px solid #fff; border-radius: 8px; background-color: #292929; padding: 5px;' id='sqlNote"+this.value+"' class='collapse caption'><img src='https://res.cloudinary.com/spidiritsu/image/upload/v1480345800/Niemiecki_czasy_"+this.value+".jpg' style='border-radius: 8px;' class='img-responsive' /><div class='caption'><h4>Notes Image "+this.value+"</h4></div><br/>");
-    $("#calloutContainer").append('<button type="button" class="btn btn-default btn-block btn-outline btnShow" data-toggle="collapse" data-target="#sqlNote'+this.value+'">SQL Notes Image '+this.value+' - SHOW / HIDE</button>');
+    $("#mainContainer").append("<div style='margin: 20px 10px; border: 2px solid #fff; border-radius: 8px; background-color: #292929; padding: 5px;' id='sqlNote"+this.value+"' class='collapse caption'><img src='https://res.cloudinary.com/spidiritsu/image/upload/v1480345800/Niemiecki_czasy_"+this.value+".jpg' style='border-radius: 8px;' class='img-responsive' /><div class='caption'><h4>German Notes Image "+this.value+"</h4></div><br/>");
+    $("#calloutContainer").append('<button type="button" class="btn btn-default btn-block btn-outline btnShow" data-toggle="collapse" data-target="#sqlNote'+this.value+'">German Notes Image '+this.value+' - SHOW / HIDE</button>');
   });
 });
